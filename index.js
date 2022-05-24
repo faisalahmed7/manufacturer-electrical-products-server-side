15df6cf5fd41e41c24569c3870427139cb4c9ab8
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-05-24 23:08:10 +0600

    Products Data Loaded Successfully

--- index.js
+++ index.js
@@ -2,13 +2,38 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.98ofi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('manufacturerProducts').collection('products');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+
+
+
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 
 
