7e4b91b9147f0e9b7f8fa251f1e333e6bb6d702c
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-05-24 23:38:45 +0600

    Single Product Details Loaded Successfully

--- index.js
+++ index.js
@@ -24,7 +24,12 @@ async function run() {
             res.send(products)
         })
 
-
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const product = await productCollection.findOne(query)
+            res.send(product)
+        })
 
     }
 
