af9eb1e3caa12a1ed9f3eb4793ebf7e3e709b943
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-05-25 23:35:37 +0600

    Ordered Data Loaded Successfully

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db('manufacturerProducts').collection('products');
+        const orderCollection = client.db('manufacturerProducts').collection('orders');
 
         app.get('/product', async (req, res) => {
             const query = {};
@@ -29,6 +30,33 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const product = await productCollection.findOne(query)
             res.send(product)
+        })
+        app.post('/order', async (req, res) => {
+            const newOrder = req.body;
+            const result = await orderCollection.insertOne(newOrder);
+            res.send(result)
+        })
+
+        // app.put('/product/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const updateQuantity = parseInt(req.body);
+        //     const filter = { _id: ObjectId(id) };
+        //     const options = { upsert: true }
+        //     const updatedDoc = {
+        //         $set: {
+        //             quantity: updateQuantity.quantity,
+
+        //         }
+        //     }
+        //     const result = await productCollection.updateOne(filter, updatedDoc, options)
+        //     res.send(result)
+        // })
+
+        app.get('/order', async (req, res) => {
+            const query = {};
+            const cursor = orderCollection.find(query)
+            const orders = await cursor.toArray()
+            res.send(orders)
         })
 
     }
